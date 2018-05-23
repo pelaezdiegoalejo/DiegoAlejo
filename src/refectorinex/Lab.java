0c40c311385743916d8e22d2ca4bac2baa9e2156
1695812 <1695812@LI-S064-0T06>
2018-05-23 09:14:47 -0400

    1er

--- src/refectorinex/Lab.java
+++ src/refectorinex/Lab.java
@@ -10,5 +10,33 @@ package refectorinex;
  * @author 1695812
  */
 public class Lab {
-    
+   String idClass;
+   int capacity;
+   int nbComputers;
+
+    public String getIdClass() {
+        return idClass;
+    }
+
+    public void setIdClass(String idClass) {
+        this.idClass = idClass;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getNbComputers() {
+        return nbComputers;
+    }
+
+    public void setNbComputers(int nbComputers) {
+        this.nbComputers = nbComputers;
+    }
+   
+
 }
