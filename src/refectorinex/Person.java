e98c09d28987371a04895cf8b12c56eef042380b
1695812 <1695812@LI-S064-0T06>
2018-05-23 08:56:13 -0400

    1er

---
+++ src/refectorinex/Person.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refectorinex;
+
+/**
+ *
+ * @author 1695812
+ */
+public class Person {
+    
+}
