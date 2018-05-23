d5508dfc8391d765a5cc1a69dfefbe94a4ba3670
1695812 <1695812@LI-S064-0T06>
2018-05-23 09:58:10 -0400

    lab refactoring

--- src/refectorinex/Person.java
+++ src/refectorinex/Person.java
@@ -5,13 +5,71 @@
  */
 package refectorinex;
 
+import java.util.Date;
+
 /**
  *
  * @author 1695812
  */
 public class Person {
-    int age;
-    public void study(){
-        System.out.println("I study.");
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
     }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the dateBirth
+     */
+    public Date getDateBirth() {
+        return dateBirth;
+    }
+
+    /**
+     * @param dateBirth the dateBirth to set
+     */
+    public void setDateBirth(Date dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    private int age;
+    private Date dateBirth;
+    private String firstName;
+    private String lastName;
 }
