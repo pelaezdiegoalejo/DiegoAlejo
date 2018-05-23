36fc7e5453a7934e62590a53cb4e58a6629135db
1695812 <1695812@LI-S064-0T06>
2018-05-23 09:36:25 -0400

    classRoom superclass of lab created

---
+++ src/refectorinex/ClassRoom.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package refectorinex;
+
+/**
+ *
+ * @author 1695812
+ */
+public class ClassRoom {
+    
+    String idClass;
+    int capacity;
+
+    public String getIdClass() {
+        return idClass;
+    }
+
+    public void setIdClass(String idClass) {
+        this.idClass = idClass;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
+}
